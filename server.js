c4b7eda856c36d503f7b08e93a6290a944ecb96f
kashif naseem <56063644+kashifnaseem50@users.noreply.github.com>
2021-11-15 14:54:35 +0500

    working

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config()
 // })
 
 const app = express()
-app.get('/', (req, res) => res.send('hello server- test 52'))
+app.get('/', (req, res) => res.send('hello server- client testing'))
 
 appMiddlewares(app)
 
