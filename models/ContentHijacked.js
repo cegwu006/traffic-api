fc9bb5369f9258aa04134d9879d4da3dcda188f5
devcloudstudio <87946904+devcloudstudio@users.noreply.github.com>
2021-11-21 23:18:12 +0300

    Update ContentHijacked.js

--- models/ContentHijacked.js
+++ models/ContentHijacked.js
@@ -8,9 +8,15 @@ const contentHijackedSchema = new Schema({
      link: { type: String, required: true },   
      message: { type: String, },   
      title: {type: String, required: true},
-     messageStyle: {type: String}
-})
+     messageStyle: {type: String},
+     createdBy:{
+          type: mongoose.Types.ObjectId, 
+          ref: 'User',
+          required: [true, 'User must be included']
+     }
+}, {timestamps: true})
 
 
 export default mongoose.model('Content', contentHijackedSchema)
+
 
