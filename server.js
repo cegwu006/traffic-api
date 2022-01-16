3dbfe5972594351f948684131037b91be5517e6e
aliGaxanfar <89012214+aliGaxanfar@users.noreply.github.com>
2022-01-16 21:20:22 +0500

    Hello world

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 
 const app = express()
-app.get('/', (req, res) => res.send('Hello, final-pipeline test-1'))
+app.get('/', (req, res) => res.send('Hello world!'))
 
 
 appMiddlewares(app)
