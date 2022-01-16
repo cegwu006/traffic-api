0dc08db16489eb162bc46d76c798559144b91859
aliGaxanfar <89012214+aliGaxanfar@users.noreply.github.com>
2022-01-16 12:51:32 +0500

    final test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 
 const app = express()
-app.get('/', (req, res) => res.send('Hello new world 1'))
+app.get('/', (req, res) => res.send('Hello, final-pipeline test'))
 
 
 appMiddlewares(app)
